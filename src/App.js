3c7fa17f47a4833a4d412c9e2b9c7aea0c838877
boris bataini <borisbataini@gmail.com>
2025-02-02 01:07:23 +0100

    lolo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+import "semantic-ui-css/semantic.min.css";
+
+import { Header, Grid, GridColumn, GridRow } from "semantic-ui-react";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>       
-          La Balancelle Formation
-        </p>
+
+        <Grid columns={3} divided>
+          <GridRow>
+            <Header  style={{ color : '#BC3A3A', fontFamily: 'Pangolin, cursive' }}>
+              La Balancelle Formation
+            </Header>
+          </GridRow>
+
+          <GridRow style={{ color : '#BC3A3A', fontFamily: 'Pangolin, cursive' }}>
+            <GridColumn>
+              <GridRow >Bérénice GUDIN</GridRow>
+              <GridRow>06 06 06 06 06</GridRow>
+            </GridColumn>
+            <GridColumn>
+              <GridRow>Bérénice GUDIN</GridRow>
+              <GridRow>06 06 06 06 06</GridRow>    </GridColumn>
+            <GridColumn>
+              <GridRow>Bérénice GUDIN</GridRow>
+              <GridRow>06 06 06 06 06</GridRow>
+            </GridColumn>
+          </GridRow>
+        </Grid>
       </header>
     </div>
   );
